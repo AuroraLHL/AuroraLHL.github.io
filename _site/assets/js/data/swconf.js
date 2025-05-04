c71add133f1bc98366f22198066ab1e875f250fc
Aurora <2074375758@qq.com>
2025-05-04 13:39:09 +0800

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721975552',resources: [
+    cacheName: 'chirpy-1746337048',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
