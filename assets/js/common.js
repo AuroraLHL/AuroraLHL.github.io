4a68240eff300e3f1e34719e7a392a24127fd2b0
Aurora <2074375758@qq.com>
2026-04-04 14:31:38 +0800

    feat: add synchronized sidebar toc for blog posts

--- assets/js/common.js
+++ assets/js/common.js
@@ -19,16 +19,124 @@ $(document).ready(function () {
 
   // bootstrap-toc
   if ($("#toc-sidebar").length) {
-    // remove related publications years from the TOC
-    $(".publications h2").each(function () {
-      $(this).attr("data-toc-skip", "");
-    });
     var navSelector = "#toc-sidebar";
     var $myNav = $(navSelector);
-    Toc.init($myNav);
+    var $tocScope = $("#markdown-content");
+
+    // Keep publication year headings out of shared TOC generation.
+    $(".publications h2").attr("data-toc-skip", "");
+
+    if ($tocScope.length) {
+      // Blog posts only expose markdown h1/h2 in the sidebar TOC.
+      $tocScope.find("h3, h4, h5, h6").attr("data-toc-skip", "");
+      Toc.init({
+        $nav: $myNav,
+        $scope: $tocScope,
+      });
+    } else {
+      Toc.init($myNav);
+    }
+
+    var $topLevelItems = $myNav.children("ul").children("li").has("ul");
+
+    function getTocSectionItem($link) {
+      var $parentSection = $link.closest("ul").closest("li");
+      return $parentSection.length ? $parentSection : $link.closest("li");
+    }
+
+    function expandTocSection($section) {
+      $topLevelItems.removeClass("toc-expanded");
+      if ($section && $section.length) {
+        $section.addClass("toc-expanded");
+      }
+    }
+
+    function getHeadingElement(hash) {
+      if (!hash || hash.charAt(0) !== "#") {
+        return null;
+      }
+
+      return document.getElementById(decodeURIComponent(hash.slice(1)));
+    }
+
+    function expandTocForHash(hash) {
+      if (!hash) {
+        return;
+      }
+
+      var $targetLink = $myNav.find("a").filter(function () {
+        return $(this).attr("href") === hash;
+      });
+
+      if ($targetLink.length) {
+        expandTocSection(getTocSectionItem($targetLink.first()));
+      }
+    }
+
+    var tocHeadingEntries = $myNav
+      .find("a")
+      .map(function () {
+        var $link = $(this);
+        var heading = getHeadingElement($link.attr("href"));
+
+        if (!heading) {
+          return null;
+        }
+
+        return {
+          $link: $link,
+          $section: getTocSectionItem($link),
+          heading: heading,
+        };
+      })
+      .get();
+
+    function expandTocForScroll() {
+      if (!tocHeadingEntries.length) {
+        return;
+      }
+
+      var activeEntry = tocHeadingEntries[0];
+      var scrollOffset = 96;
+
+      tocHeadingEntries.forEach(function (entry) {
+        if (entry.heading.getBoundingClientRect().top <= scrollOffset) {
+          activeEntry = entry;
+        }
+      });
+
+      expandTocSection(activeEntry.$section);
+    }
+
+    var isTocScrollSyncQueued = false;
+
+    function queueTocScrollSync() {
+      if (isTocScrollSyncQueued) {
+        return;
+      }
+
+      isTocScrollSyncQueued = true;
+      window.requestAnimationFrame(function () {
+        isTocScrollSyncQueued = false;
+        expandTocForScroll();
+      });
+    }
+
+    $myNav.on("click", "a", function () {
+      expandTocSection(getTocSectionItem($(this)));
+    });
+
+    expandTocForHash(window.location.hash);
+    $(window).on("hashchange", function () {
+      expandTocForHash(window.location.hash);
+    });
+    $(window).on("scroll resize", queueTocScrollSync);
+
     $("body").scrollspy({
       target: navSelector,
     });
+
+    queueTocScrollSync();
   }
 
   // add css to jupyter notebooks
